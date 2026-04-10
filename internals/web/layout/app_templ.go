edacdf9aff4eb39a76ae58eaffecbe5caf42e4bd
devitodbug <davidochiengy@gmail.com>
2026-04-10 12:18:47 +0300

    feat: add favicon to layout and update HTML template for improved branding

--- internals/web/layout/app_templ.go
+++ internals/web/layout/app_templ.go
@@ -36,7 +36,7 @@ func App() templ.Component {
 			{Text: "Blogs", Link: "/blogs"},
 			{Text: "Resume", Link: "/resume"},
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><title>David</title><link href=\"/static/css/style.css\" rel=\"stylesheet\"></head><body style=\"font-family: 'EB Garamond', serif;\" class=\"flex w-full bg-yellow-50 flex-col items-center pb-5\"><div class=\"w-full lg:w-[45%] p-4\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><title>David</title><link rel=\"icon\" type=\"image/x-icon\" href=\"/static/assets/favicon.ico\"><link href=\"/static/css/style.css\" rel=\"stylesheet\"></head><body style=\"font-family: 'EB Garamond', serif;\" class=\"flex w-full bg-yellow-50 flex-col items-center pb-5\"><div class=\"w-full lg:w-[45%] p-4\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
