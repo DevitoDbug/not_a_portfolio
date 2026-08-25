3171d7566e62099ce29851e9c07aaf7ffb290e93
devitodbug <davidochiengy@gmail.com>
2026-08-25 10:52:30 +0300

    chore: clean ups

--- internals/api/resume.go
+++ internals/api/resume.go
@@ -44,7 +44,7 @@ func (a *Api) ResumeHandler(w http.ResponseWriter, r *http.Request) {
 		},
 		{
 			JobName:    "Sakah | Part-time",
-			TimePeriod: "Feb 2021 – Jan 2025",
+			TimePeriod: "Feb 2021 – May 2024",
 			Links: []pages.Link{
 				"https://sakah.co/",
 				"https://www.llamaevents.com/",
